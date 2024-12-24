99746400cd2d78aae92081c0b44b92ee475c3387
Bharath <bharath.b@irax.in>
2024-12-24 15:55:22 +0530

    first commit

---
+++ abc.ts
@@ -0,0 +1,8 @@
+export let a : number=10;
+export function dis(){
+    console.log("This is disc1 functionpart of abc module")
+}   
+
+export function disc() {
+    console.log("this is disc2 functionari of abc module")
+}
