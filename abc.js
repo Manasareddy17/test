d832ab62357eba5909f9f6ed50e8a966c2e243c8
Bharath <bharath.b@irax.in>
2024-12-26 13:20:45 +0530

    added abc.js

---
+++ abc.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.a = void 0;
+exports.dis = dis;
+exports.disc = disc;
+exports.a = 10;
+function dis() {
+    console.log("This is disc1 functionpart of abc module");
+}
+function disc() {
+    console.log("this is disc2 functionari of abc module");
+}
